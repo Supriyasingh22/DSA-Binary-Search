eb2b4e54a701d76b22396eff287da8fb1930783c
supriya Kumari <supriasingh12823@gmail.com>
2023-05-02 09:59:12 +0530

    Median of row-wise sorted matrix is added

---
+++ MedianOfRow-WiseSorted-Matrix.cpp
@@ -0,0 +1,81 @@
+// To find median of row-wise sorted matrix 
+
+#include<iostream>
+using namespace std ;
+
+int TotalLessEle(int matrix[200][200],int r,int c,int target)
+{
+    int count = 0 ;
+
+    for(int i =0 ;i<r;i++)
+    {
+        int lb = 0 ;
+        int ub = c-1;
+        int mid ;
+        while(lb<=ub)
+        {
+            mid = lb+(ub-lb)/2 ;
+            if(matrix[i][mid]<=target)
+            {
+                lb = mid+1 ;
+            }
+            else 
+            {
+                ub = mid-1 ;
+            }
+        }
+        count+=lb ;
+
+
+    }
+    return count ;
+
+}
+
+int findMedian(int r,int c,int matrix[200][200])
+{
+    int lb = 0 ;
+    int ub = 2000 ;
+    int assumeMed ;
+    int N = r*c ;
+    while(lb<=ub)
+    {
+        assumeMed = lb+(ub-lb)/2 ;
+        // count total no. of elements lesser than assumed median 
+        int count = TotalLessEle(matrix,r,c,assumeMed) ;
+        if(count<=N/2)
+        {
+            lb = assumeMed+1 ;
+        }
+        else 
+        {
+            ub = assumeMed-1;
+        }
+
+    }
+
+    return lb ;
+
+}
+
+int main()
+{
+    int r , c ;
+    cout<<"Enter the rows and columns : ";
+    cin>>r>>c ;
+
+    int matrix[200][200];
+
+    cout<<"Enter eleme ts inside the matrix : ";
+    for(int i = 0 ;i<r;i++)
+    {
+        for(int j = 0 ;j<c;j++)
+        {
+            cin>>matrix[i][j];
+        }
+    }
+
+    int res = findMedian(r,c,matrix);
+    cout<<"median is :"<<res ;
+    return 0 ;
+}
